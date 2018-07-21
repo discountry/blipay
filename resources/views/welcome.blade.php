da08584528342d52273dac11380418dfc32edd55
discountry <331027793@qq.com>
2018-07-22 03:05:04 +0800

    no debug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
         <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
         <script type="text/javascript" charset="utf-8">
-            wx.config(<?php echo $app->jssdk->buildConfig(array('scanQRCode'), true) ?>);
+            wx.config(<?php echo $app->jssdk->buildConfig(array('scanQRCode'), false) ?>);
         </script>
     </head>
     <body>
